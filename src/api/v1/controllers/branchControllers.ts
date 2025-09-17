8a33121d72eda758d5be8b32837c15e60d6a0e62
Lalaine Balmediano <lbalmediano3@academic.rrc.ca>
2025-09-17 09:28:04 -0500

    Implemented getAllBranches and getBranchesByID controllers

--- src/api/v1/controllers/branchControllers.ts
+++ src/api/v1/controllers/branchControllers.ts
@@ -2,3 +2,36 @@ import { Request, Response, NextFunction } from "express";
 import * as branchServices from "../services/branchServices";
 import { branches } from "../../../data/branches";
 import { Branch } from "../models/models";
+
+export const getAllBranches = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const branch: Branch[] = await branchServices.getAllBranches();
+        res.status(200).json({ 
+            message: "Branches retrieved successfully", 
+            data: branch,
+        });
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+export const getBranchByID = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const id: number = parseInt(req.params.id); 
+        const branch = await branchServices.getBranchByID(id);
+         res.status(200).json({
+            message: "Get an employee",
+            data: branch,
+        });
+    } catch (error: unknown) {
+        next(error);
+    }
+};
