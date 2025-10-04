079e98ee2f349d1bae8d39b0847083703abe757f
Lalaine Balmediano <lbalmediano3@academic.rrc.ca>
2025-10-04 18:26:41 -0500

    Created employees and branches json file and imprted it to firestore database

--- importData.js
+++ importData.js
@@ -1,17 +1,19 @@
 // import firebase-admin and data files
 const admin = require('firebase-admin');
-const { employees } = require('./src/data/employees');
-const { branches } = require('./src/data/branches');
+const employees = require('./src/data/employees.json');
+const branches = require('./src/data/branches.json');
 
 admin.initializeApp({ credential: admin.credential.cert('bed-assignment-3-801a3-firebase-adminsdk-fbsvc-ee05930002.json') });
 const db = admin.firestore();
 
 // Import employees
 employees.forEach(async (employee) => {
-  await db.collection('employees').add(employee);
+  // To import employees data and keep the employee id
+  await db.collection('employees').doc(employee.id).set(employee);
 });
 
 // Import branches
 branches.forEach(async (branch) => {
-  await db.collection('branches').add(branch);
+  // To import branches data and keep the branch id
+  await db.collection('branches').doc(branch.id).set(branch);
 });
