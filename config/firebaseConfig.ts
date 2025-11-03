178e07771513f9e38877d14e158a0306137e973c
Lalaine Balmediano <lbalmediano3@academic.rrc.ca>
2025-11-03 14:58:31 -0600

    Fixed minor Auth error

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -70,6 +70,9 @@ const initializeFirebaseAdmin = (): App => {
 // Initialize the Firebase Admin app
 const firebaseApp: App = initializeFirebaseAdmin();
 
+// Get a reference to firebase authentication
+const auth: Auth = getAuth(firebaseApp);
+
 // Get a reference to the firestore database
 const db: Firestore = getFirestore(firebaseApp);
 
